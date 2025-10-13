379694c28cb13ae8bede717df26baabdea32d532
Adrian Gonzalez <trabajoadrian247@gmail.com>
2025-10-13 21:40:59 +0200

    He metido cada sección del html principal en un componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { HeroSection } from './components/hero-section/hero-section';
+import { NewsSection } from "./components/news-section/news-section";
+import { ProjectSection } from "./components/project-section/project-section";
+import { TechSection } from "./components/tech-section/tech-section";
+import { PartnersSection } from "./components/partners-section/partners-section";
+import { Cta } from "./components/cta/cta";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, HeroSection],
+  imports: [Header, HeroSection, NewsSection, ProjectSection, TechSection, PartnersSection, Cta, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
